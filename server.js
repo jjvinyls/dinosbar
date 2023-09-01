05b333a860868c724b66f15ec86fa6da45b21b60
harpoon js <w3st.io2021@gmail.com>
2023-09-01 13:14:36 -0400

    Remove unused packages

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const express = require('express')
 const http = require('http')
-const mongoose = require('mongoose')
 const path = require('path')
 
 
@@ -14,21 +13,6 @@ const config = require('./s-config')
 // [EXPRESS] //
 const app = express()
 const server = http.createServer(app)
-
-
-// [MONGOOSE-CONNECTION] //
-mongoose.connect(
-	config.MONG_URI,
-	{
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useFindAndModify: false,
-	},
-	(err, connected) => {
-		if (connected) { console.log('Mongoose Connected to DB') }
-		else { console.log(`Mongoose Connection Error --> ${err}`) }
-	}
-)
 
 
 // [USE] // Default Stuff // Set static Folder // Rate-Limiter //
